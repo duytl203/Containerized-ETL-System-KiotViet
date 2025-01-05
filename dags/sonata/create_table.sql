8f35cebaa3dc7c436b01c83e9ee93ce51e9fe4c0
duytl203 <tlduy203@gmail.com>
2025-01-05 12:09:28 +0700

    Update create_table.sql

--- dags/sonata/create_table.sql
+++ dags/sonata/create_table.sql
@@ -1,45 +1,43 @@
 CREATE TABLE customers (
     customer_id VARCHAR PRIMARY KEY,
-    customer_name VARCHAR,
-    created_date TIMESTAMP,
+    customer_name VARCHAR NOT NULL,
+    created_date TIMESTAMP NOT NULL,
     updated_date TIMESTAMP
 );
-
 CREATE TABLE products (
     id VARCHAR PRIMARY KEY,
-    name VARCHAR,
+    name VARCHAR NOT NULL,
     category_id NUMERIC,
     original_price NUMERIC,
-    created_date TIMESTAMP,
+    created_date TIMESTAMP NOT NULL,
     unit VARCHAR,
     updated_date TIMESTAMP
 );
-
 CREATE TABLE categories (
-    id NUMERIC PRIMARY KEY,
-    name VARCHAR,
-    created_date TIMESTAMP,
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    created_date TIMESTAMP NOT NULL,
     updated_date TIMESTAMP
 );
-
 CREATE TABLE branches (
-    id NUMERIC PRIMARY KEY,
-    name VARCHAR,
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
     address VARCHAR,
     location_name VARCHAR,
     contact_number VARCHAR,
-    created_date TIMESTAMP,
+    created_date TIMESTAMP NOT NULL,
     updated_date TIMESTAMP
 );
-
 CREATE TABLE invoices (
     id VARCHAR PRIMARY KEY,
-    created_date TIMESTAMP,
-    branch_id NUMERIC,
+    created_date TIMESTAMP NOT NULL,
+    branch_id NUMERIC NOT NULL,
     staff_name VARCHAR,
     revenue NUMERIC,
     total_payment NUMERIC,
-    status_value VARCHAR,
+    statusValue VARCHAR,
     updated_date TIMESTAMP,
-    invoice_details JSONB
+    invoiceDetails JSONB
 );
+GRANT ALL PRIVILEGES ON invoices,customers,products,branches,categories TO duytl;
+
