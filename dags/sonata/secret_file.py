2ed2b6aca9be008735c886a10fd81f8f25d8ff94
duytl203 <tlduy203@gmail.com>
2025-01-05 12:11:39 +0700

    Create secret_file.py

---
+++ dags/sonata/secret_file.py
@@ -0,0 +1,8 @@
+def get_access_token():
+    access_token = '***************'  
+    retail_name = '***************'
+    return access_token, retail_name
+
+def get_db_url():
+    db_url = 'postgresql+psycopg2://duytl:********@********:********/********'
+    return db_url
